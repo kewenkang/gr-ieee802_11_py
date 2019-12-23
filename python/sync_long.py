999896a944bb0caecd1440517d8e5800c19ef9a6
kewenkang <1528428728@qq.com>
2019-12-23 11:21:06 +0800

    sync short

--- python/sync_long.py
+++ python/sync_long.py
@@ -20,23 +20,64 @@
 # 
 
 import numpy
+from enum import Enum
 from gnuradio import gr
+import utils
 
-class sync_long(gr.sync_block):
+class State(Enum):
+    SYNC = 1
+    COPY = 2
+    RESET = 3
+
+LONG = numpy.array([
+complex(-0.0455, -1.0679), complex( 0.3528, -0.9865), complex( 0.8594,  0.7348), complex( 0.1874,  0.2475),
+complex( 0.5309, -0.7784), complex(-1.0218, -0.4897), complex(-0.3401, -0.9423), complex( 0.8657, -0.2298),
+complex( 0.4734,  0.0362), complex( 0.0088, -1.0207), complex(-1.2142, -0.4205), complex( 0.2172, -0.5195),
+complex( 0.5207, -0.1326), complex(-0.1995,  1.4259), complex( 1.0583, -0.0363), complex( 0.5547, -0.5547),
+complex( 0.3277,  0.8728), complex(-0.5077,  0.3488), complex(-1.1650,  0.5789), complex( 0.7297,  0.8197),
+complex( 0.6173,  0.1253), complex(-0.5353,  0.7214), complex(-0.5011, -0.1935), complex(-0.3110, -1.3392),
+complex(-1.0818, -0.1470), complex(-1.1300, -0.1820), complex( 0.6663, -0.6571), complex(-0.0249,  0.4773),
+complex(-0.8155,  1.0218), complex( 0.8140,  0.9396), complex( 0.1090,  0.8662), complex(-1.3868, -0.0000),
+complex( 0.1090, -0.8662), complex( 0.8140, -0.9396), complex(-0.8155, -1.0218), complex(-0.0249, -0.4773),
+complex( 0.6663,  0.6571), complex(-1.1300,  0.1820), complex(-1.0818,  0.1470), complex(-0.3110,  1.3392),
+complex(-0.5011,  0.1935), complex(-0.5353, -0.7214), complex( 0.6173, -0.1253), complex( 0.7297, -0.8197),
+complex(-1.1650, -0.5789), complex(-0.5077, -0.3488), complex( 0.3277, -0.8728), complex( 0.5547,  0.5547),
+complex( 1.0583,  0.0363), complex(-0.1995, -1.4259), complex( 0.5207,  0.1326), complex( 0.2172,  0.5195),
+complex(-1.2142,  0.4205), complex( 0.0088,  1.0207), complex( 0.4734, -0.0362), complex( 0.8657,  0.2298),
+complex(-0.3401,  0.9423), complex(-1.0218,  0.4897), complex( 0.5309,  0.7784), complex( 0.1874, -0.2475),
+complex( 0.8594, -0.7348), complex( 0.3528,  0.9865), complex(-0.0455,  1.0679), complex( 1.3868, -0.0000),
+])
+
+class sync_long(gr.basic_block):
     """
     docstring for block sync_long
     """
     def __init__(self, sync_length, verbose):
-        gr.sync_block.__init__(self,
+        gr.basic_block.__init__(self,
             name="sync_long",
-            in_sig=[numpy.float],
-            out_sig=[numpy.float])
+            in_sig=[numpy.complex64, numpy.complex64],
+            out_sig=[numpy.complex64])
+        self.d_state = State.SYNC
+        self.d_offset = 0
+        self.d_logger = self.d_logger = utils.get_logger('sync_long', verbose)
+        self.d_sync_length = sync_length
+        self.d_fir = gr.filter.kernel.fir_filter_ccc(1, LONG)
 
+        set_tag_propagation_policy(gr.block.TPP_DONT)
 
-    def work(self, input_items, output_items):
+    def general_work(self, input_items, output_items):
         in0 = input_items[0]
+        in_delayed = input_items[1]
         out = output_items[0]
         # <+signal processing here+>
+        self.d_logger.debug('LONG ninput[0]: {}, ninput[1]: {}, noutput: {}, state: {}'.format(
+            len(input_items[0]), len(input_items[1]), len(output_items[0]), self.d_state))
+
+        ninput = min(min(len(input_items[0]), len(input_items[1])), 8192)
+        nread = nitems_read(0)
+        tags = get_tags_in_range(0, nread, nread + ninput)
+        if len(tags) > 0:
+            sorted()
         out[:] = in0
         return len(output_items[0])
 
--- python/sync_short.py
+++ python/sync_short.py
@@ -21,6 +21,7 @@
 
 import numpy
 from gnuradio import gr
+import pmt
 import utils
 from enum import Enum
 
@@ -28,29 +29,93 @@ class State(Enum):
     SEARCH = 1
     COPY = 2
 
-class sync_short(gr.block):
+MIN_GAP = 480
+MAX_SAMPLES = 540 * 80
+
+class sync_short(gr.basic_block):
     """
     docstring for block sync_short
     """
     def __init__(self, threshold, min_plateau, verbose):
-        gr.block.__init__(self,
+        gr.basic_block.__init__(self,
             name="sync_short",
             in_sig=[numpy.complex64, numpy.complex64, numpy.float],
             out_sig=[numpy.complex64])
-        self.logger = utils.get_logger('sync_short', verbose)
-        self.threshold = threshold
-        self.min_plateau = min_plateau
-        self.state = State.SEARCH
-        self.plateau = 0
-        self.freq_offset = 0
-        self.copied = 0
-        set_tag_propagation_policy(gr.block.)
+        self.d_logger = utils.get_logger('sync_short', verbose)
+        self.d_threshold = threshold
+        self.d_min_plateau = min_plateau
+        self.d_state = State.SEARCH
+        self.d_plateau = 0
+        self.d_freq_offset = 0
+        self.d_copied = 0
+        # set_tag_propagation_policy(gr.block.)
 
+    def insert_tag(self, item, freq_offset, input_item):
+        self.d_logger.debug('frame start at in: {} out: {}'.format(input_item, item))
 
-    def work(self, input_items, output_items):
+        key = pmt.intern('wifi_start')
+        value = pmt.from_double(freq_offset)
+        srcid = pmt.intern(name())
+        add_item_tag(0, item, key, value, srcid)
+
+    def general_work(self, input_items, output_items):
         in0 = input_items[0]
+        in_abs = input_items[1]
+        in_cor = input_items[2]
         out = output_items[0]
+
+        noutput = len(output_items[0])
+        ninput = min(min(len(input_items[0]), len(input_items[1])), len(input_items[2]))
+
+        if self.d_state == State.SEARCH:
+            i = 0
+            while i < ninput:
+                if in_cor[i] > self.d_threshold:
+                    if self.d_plateau < self.d_min_plateau:
+                        self.d_plateau += 1
+                    else:
+                        self.d_state = State.COPY
+                        self.d_copied = 0
+                        self.d_plateau = 0
+                        self.d_freq_offset = numpy.angle(in_abs[i]) / 16
+                        self.insert_tag(nitems_written(0), self.d_freq_offset, nitems_read(0)+i)
+                        self.d_logger.info('SHORT Frame!')
+                        break
+                else:
+                    self.d_plateau = 0
+                i += 1
+            consume_each(i)
+            return 0
+        elif self.d_state == State.COPY:
+            o = 0
+            while o < ninput and o < noutput and self.d_copied < MAX_SAMPLES:
+                if in_cor[o] > self.d_threshold:
+                    if self.d_plateau < self.d_min_plateau:
+                        self.d_plateau += 1
+                    # there is another frame
+                    elif self.d_copied > MIN_GAP:
+                        # self.d_state = State.COPY
+                        self.d_copied = 0
+                        self.d_plateau = 0
+                        self.d_freq_offset = numpy.angle(in_abs[o]) / 16
+                        self.insert_tag(nitems_written(0) + o, self.d_freq_offset, nitems_read(0) + o)
+                        self.d_logger.info('SHORT Frame!')
+                        break
+                else:
+                    self.d_plateau = 0
+
+                out[o] = in0[o] * numpy.exp(complex(0, - self.d_freq_offset * self.d_copied))
+                o += 1
+                self.d_copied += 1
+            if self.d_copied == MAX_SAMPLES:
+                self.d_state = State.SEARCH
+            self.d_logger.debug('SHORT copied: {}'.format(o))
+            consume_each(o)
+            return o
+
+        raise Exception('sync short: unknown state')
         # <+signal processing here+>
-        out[:] = in0
-        return len(output_items[0])
+
+        # out[:] = in0
+        # return len(output_items[0])
 
